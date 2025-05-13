46dd26f9a2e77b6a396d71d6beb99fd4bbca9cfc
erimii <990303amy88@gmail.com>
2025-05-13 12:43:17 +0900

    SQLite → MySQL 이전 완료

--- backend/ifitv/settings.py
+++ backend/ifitv/settings.py
@@ -77,14 +77,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ifitv.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ifitv_db',
+        'USER': 'root',
+        'PASSWORD': 'rubi',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
